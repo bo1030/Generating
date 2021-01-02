69a2511d8759a2540606b69f6a69c0546622d5c2
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-01-02 21:43:49 +0900

    change crawler to gather L2 maps

--- Web_crawler/crawler.py
+++ Web_crawler/crawler.py
@@ -1,22 +1,23 @@
 from urllib.request import urlopen
+import ssl
 
 baseUrl = "https://map0.daumcdn.net/"
 midadrr = ["map_skyview", "map_2d/2012tlq"]
 
 #positions
-start_y = 437
-start_x = 188
-finish_y = 521
-finish_x = 244
+start_y = 2513
+start_x = 2904
+finish_y = 2560
+finish_x = 2936
 
-n=1
+n=0
 for i in range(start_y, finish_y + 1):
     for j in range(start_x, finish_x + 1):
         for k in midadrr:
-            url = baseUrl + k + '/' + 'L5' +'/' + str(i) + '/' + str(j) + (".jpg" if k == "map_skyview" else ".png")
+            url = baseUrl + k + '/' + 'L2' +'/' + str(i) + '/' + str(j) + (".jpg" if k == "map_skyview" else ".png")
             print(url)
             with urlopen(url) as f:
-                with open( './images/L5/' + ("sky" if k == "map_skyview" else "map") + str(n) +'.png', 'wb') as h:
+                with open( './images/L2/' + ("sky" if k == "map_skyview" else "map") + str(n) +'.png', 'wb') as h:
                     img = f.read()
                     h.write(img)
         n = n + 1
